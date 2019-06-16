14ff448101c557e82cf4879ed4379b536ad2e0e0
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-06-16 21:21:16 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
   mapUnfaded.classList.remove('map--faded');
 
   var chooseRandomElement = function (arr) {
-    var randomElement = arr[Math.round(Math.random(1) * (arr.length -1))];
+    var randomElement = arr[Math.round(Math.random(1) * (arr.length - 1))];
     return randomElement;
   };
 
@@ -16,7 +16,7 @@
 
   measureWidthMap('.map__pins');
 
-  var createMock = function() {
+  var createMock = function () {
     var types = ['palace', 'flat', 'house', 'bungalo'];
     var announcement = [];
 
@@ -32,7 +32,7 @@
           x: Math.round(Math.random() * measureWidthMap('.map__pins')),
           y: Math.round(Math.random() * 500 + 130)
         }
-      }
+      };
     }
     return announcement;
   };
@@ -46,7 +46,7 @@
 
     for (var i = 0; i < mock.length; i++) {
       var pin = similarPin.cloneNode(true);
-      pin.setAttribute('style','left: ' + mock[i].location.x + 'px; top: ' + mock[i].location.y + 'px;');
+      pin.setAttribute('style', 'left: ' + mock[i].location.x + 'px; top: ' + mock[i].location.y + 'px;');
       pin.querySelector('img').setAttribute('src', mock[i].author.avatar);
       pin.querySelector('img').setAttribute('alt', mock[i].offer.type);
 
