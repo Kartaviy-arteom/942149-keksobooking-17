e36012a0cad9e802e99042fa3687727a99131f64
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-06-18 15:30:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@
     return elementSizes;
   };
 
-  var renderTestPin = function (childElement, parentElement) {
+  var getPinSize = function (childElement, parentElement) {
     var pin = childElement.cloneNode(true);
     pin.setAttribute('style', 'visibility: hidden;');
     parentElement.appendChild(pin);
@@ -35,7 +35,7 @@
   var createMock = function (childElement, parentElement) {
     var types = ['palace', 'flat', 'house', 'bungalo'];
     var announcement = [];
-    var pinSizes = renderTestPin(childElement, parentElement);
+    var pinSizes = getPinSize(childElement, parentElement);
     var mapX = measureElement(parentElement).x;
 
     for (var i = 0; i < 8; i++) {
@@ -47,33 +47,36 @@
           type: chooseRandomElement(types)
         },
         location: {
-          x: Math.floor(Math.random() * (mapX - pinSizes.x)),
-          y: Math.floor(Math.random() * 500 + 130 - pinSizes.y)
+          x: Math.floor(Math.random() * mapX),
+          y: Math.floor(Math.random() * 500 + 130)
         }
       };
     }
     return announcement;
   };
 
-  var fragment = document.createDocumentFragment();
-  var renderPin = function (i) {
-    var mock = createMock(similarPin, similarListElement);
+  var pinSizes = getPinSize(similarPin, similarListElement);
+
+
+  var renderPin = function (i, pinOffset, childElement, parentElement) {
+    var mock = createMock(childElement, parentElement);
     var pin = similarPin.cloneNode(true);
     var pinImage = pin.querySelector('img');
 
-    pin.setAttribute('style', 'left: ' + mock[i].location.x + 'px; top: ' + mock[i].location.y + 'px;');
+    pin.setAttribute('style', 'left: ' + (mock[i].location.x + pinOffset.x / 2) + 'px; top: ' + (mock[i].location.y - pinOffset.y) + 'px;');
     pinImage.setAttribute('src', mock[i].author.avatar);
     pinImage.setAttribute('alt', mock[i].offer.type);
 
-    fragment.appendChild(pin);
+    return pin;
   };
 
-  var insertItems = function (items, renderItem, target) {
+  var insertItems = function (renderItem, target, pinOffset, childElement, parentElement) {
+    var fragment = document.createDocumentFragment();
     for (var i = 0; i < 8; i++) {
-      renderPin(i);
+      fragment.appendChild(renderItem(i, pinOffset, childElement, parentElement));
     }
-    target.appendChild(items);
+    target.appendChild(fragment);
   };
 
-  insertItems(fragment, renderPin, similarListElement);
+  insertItems(renderPin, similarListElement, pinSizes, similarPin, similarListElement);
 })();
