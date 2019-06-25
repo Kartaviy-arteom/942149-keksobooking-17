41f700229795c59558f127ffb7e32a12ee11dcd1
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-06-25 11:28:50 +0300

    валидация полей создания объявления
    Поле «Цена за ночь»;
    Поле «Тип жилья»;
    Поле «Адрес»;
    Поля «Время заезда», «Время выезда».

--- js/main.js
+++ js/main.js
@@ -146,4 +146,39 @@
     top: mainPinCoordinate.top + GAP_PIN_Y
   };
 
+  // минимальные цены объявления
+  var houseType = form.querySelector('#type'); // не понятно как осуществить поиск по id внутри nodelist formSelects
+  var priceHouse = form.querySelector('#price');
+
+  houseType.addEventListener('change', function(evt) {
+    var houseTypeIndex = houseType.options.selectedIndex;
+    if (houseType.options[houseTypeIndex].text === 'Бунгало') {
+      priceHouse.setAttribute('min', '0');
+      priceHouse.setAttribute('placeholder', '0');
+    };
+    if (houseType.options[houseTypeIndex].text === 'Квартира') {
+      priceHouse.setAttribute('min', '1000');
+      priceHouse.setAttribute('placeholder', '1000');
+    };
+    if (houseType.options[houseTypeIndex].text === 'Дом') {
+      priceHouse.setAttribute('min', '5000');
+      priceHouse.setAttribute('placeholder', '5000');
+    };
+    if (houseType.options[houseTypeIndex].text === 'Дворец') {
+      priceHouse.setAttribute('min', '10000');
+      priceHouse.setAttribute('placeholder', '10000');
+    };
+  });
+
+  // время заезда и выезда
+  var checkInTime = form.querySelector('#timein');
+  var checkOutTime = form.querySelector('#timeout');
+
+  checkInTime.addEventListener('change', function() {
+    checkOutTime.options.selectedIndex = checkInTime.options.selectedIndex;
+  });
+  checkOutTime.addEventListener('change', function() {
+    checkInTime.options.selectedIndex = checkOutTime.options.selectedIndex;
+  });
+
 })();
