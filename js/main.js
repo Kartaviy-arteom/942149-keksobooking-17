c134b43adf8c2f0b0b9a97bb8294cc66d0b14ac6
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-06-26 15:51:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,39 +147,37 @@
   };
 
   // валидация формы
-  (function () {
-    var houseType = form.querySelector('#type'); // не понятно, как осуществить поиск по id внутри nodelist formSelects
-    var priceHouse = form.querySelector('#price');
+  var houseType = form.querySelector('#type'); // не понятно, как осуществить поиск по id внутри nodelist formSelects
+  var priceHouse = form.querySelector('#price');
 
-    houseType.addEventListener('change', function () {
-      var houseTypeIndex = houseType.options.selectedIndex;
-      if (houseType.options[houseTypeIndex].text === 'Бунгало') {
-        priceHouse.setAttribute('min', '0');
-        priceHouse.setAttribute('placeholder', '0');
-      }
-      if (houseType.options[houseTypeIndex].text === 'Квартира') {
-        priceHouse.setAttribute('min', '1000');
-        priceHouse.setAttribute('placeholder', '1000');
-      }
-      if (houseType.options[houseTypeIndex].text === 'Дом') {
-        priceHouse.setAttribute('min', '5000');
-        priceHouse.setAttribute('placeholder', '5000');
-      }
-      if (houseType.options[houseTypeIndex].text === 'Дворец') {
-        priceHouse.setAttribute('min', '10000');
-        priceHouse.setAttribute('placeholder', '10000');
-      }
-    });
+  houseType.addEventListener('change', function () {
+    var houseTypeIndex = houseType.options.selectedIndex;
+    if (houseType.options[houseTypeIndex].text === 'Бунгало') {
+      priceHouse.setAttribute('min', '0');
+      priceHouse.setAttribute('placeholder', '0');
+    }
+    if (houseType.options[houseTypeIndex].text === 'Квартира') {
+      priceHouse.setAttribute('min', '1000');
+      priceHouse.setAttribute('placeholder', '1000');
+    }
+    if (houseType.options[houseTypeIndex].text === 'Дом') {
+      priceHouse.setAttribute('min', '5000');
+      priceHouse.setAttribute('placeholder', '5000');
+    }
+    if (houseType.options[houseTypeIndex].text === 'Дворец') {
+      priceHouse.setAttribute('min', '10000');
+      priceHouse.setAttribute('placeholder', '10000');
+    }
+  });
 
-    // время заезда и выезда
-    var checkInTime = form.querySelector('#timein');
-    var checkOutTime = form.querySelector('#timeout');
+  // время заезда и выезда
+  var checkInTime = form.querySelector('#timein');
+  var checkOutTime = form.querySelector('#timeout');
 
-    checkInTime.addEventListener('change', function () {
-      checkOutTime.options.selectedIndex = checkInTime.options.selectedIndex;
-    });
-    checkOutTime.addEventListener('change', function () {
-      checkInTime.options.selectedIndex = checkOutTime.options.selectedIndex;
-    });
-  })();
+  checkInTime.addEventListener('change', function () {
+    checkOutTime.options.selectedIndex = checkInTime.options.selectedIndex;
+  });
+  checkOutTime.addEventListener('change', function () {
+    checkInTime.options.selectedIndex = checkOutTime.options.selectedIndex;
+  });
 })();
