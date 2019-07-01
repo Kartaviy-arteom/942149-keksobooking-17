75cb37d699400477f19d968c35d5de962207037d
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-07-01 10:12:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -173,22 +173,24 @@
   // перемещение пина
   var activated = false;
   var restrictMovement = function () {
-    var TOP_MAP_RANGE = 630; // Хотя по сути это нижний предел!?
-    var BOTTOM_MAP_RANGE = 130;
-    var LEFT_MAP_RANGE = 0;
-    var rightMapRange = map.offsetWidth - mainPin.offsetWidth;
+    var rectangle = {
+      bottom: 630,
+      top: 130,
+      left: 0,
+      right: map.offsetWidth - mainPin.offsetWidth
+    };
 
-    if (parseInt(mainPin.style.top, 10) > TOP_MAP_RANGE) {
-      mainPin.style.top = TOP_MAP_RANGE + 'px';
+    if (parseInt(mainPin.style.top, 10) > rectangle.bottom) {
+      mainPin.style.top = rectangle.bottom + 'px';
     }
-    if (parseInt(mainPin.style.top, 10) < BOTTOM_MAP_RANGE) {
-      mainPin.style.top = BOTTOM_MAP_RANGE + 'px';
+    if (parseInt(mainPin.style.top, 10) < rectangle.top) {
+      mainPin.style.top = rectangle.top + 'px';
     }
-    if (parseInt(mainPin.style.left, 10) < LEFT_MAP_RANGE) {
-      mainPin.style.left = LEFT_MAP_RANGE;
+    if (parseInt(mainPin.style.left, 10) < rectangle.left) {
+      mainPin.style.left = rectangle.left;
     }
-    if (parseInt(mainPin.style.left, 10) > rightMapRange) {
-      mainPin.style.left = rightMapRange + 'px';
+    if (parseInt(mainPin.style.left, 10) > rectangle.right) {
+      mainPin.style.left = rectangle.right + 'px';
     }
   };
 
